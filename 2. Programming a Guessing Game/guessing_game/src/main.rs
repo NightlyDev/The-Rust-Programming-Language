a677a0b9730c6ab7a05b5087dd9e0241e189ac01
Alexander Hadula <alexander.hadula@gmail.com>
2023-01-22 05:29:00 +0100

    Add random number generator of chapter 2

--- 2. Programming a Guessing Game/guessing_game/src/main.rs
+++ 2. Programming a Guessing Game/guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);  // number generator seeded by the OS
+
+    println!("The secret number is {secret_number}");
 
     println!("Please input your guess.");
 
