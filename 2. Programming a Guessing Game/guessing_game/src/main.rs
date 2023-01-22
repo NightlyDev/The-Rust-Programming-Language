7a16ba1175691cf60b489dd9cd4c82378bf028d3
Alexander Hadula <alexander.hadula@gmail.com>
2023-01-22 05:49:25 +0100

    Add loop for guessing game

--- 2. Programming a Guessing Game/guessing_game/src/main.rs
+++ 2. Programming a Guessing Game/guessing_game/src/main.rs
@@ -9,21 +9,26 @@ fn main() {
 
     println!("The secret number is {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new(); // mut -> mutable - changeable
+        let mut guess = String::new(); // mut -> mutable - changeable
 
-    io::stdin()
-        .read_line(&mut guess) // &mut -> mutable reference to change contents of variable
-        .expect("Failed to read line"); // handles program when Result of read_line is 'Err'
+        io::stdin()
+            .read_line(&mut guess) // &mut -> mutable reference to change contents of variable
+            .expect("Failed to read line"); // handles program when Result of read_line is 'Err'
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // Shadowed a immutable variable from guess - used for conversion
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); // Shadowed a immutable variable from guess - used for conversion
 
-    println!("You guessed : {guess}");
+        println!("You guessed : {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
