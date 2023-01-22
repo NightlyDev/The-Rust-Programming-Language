740d6d539c9e5dbb8958ac169cc29123112f9698
Alexander Hadula <alexander.hadula@gmail.com>
2023-01-22 05:19:57 +0100

    Add first guessing game step

--- 2. Programming a Guessing Game/guessing_game/src/main.rs
+++ 2. Programming a Guessing Game/guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, World!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();  // mut -> mutable - changeable
+
+    io::stdin()
+        .read_line(&mut guess)      // &mut -> mutable reference to change contents of variable 
+        .expect("Failed to read line"); // handles program when Result of read_line is 'Err'
+
+    println!("You guessed : {guess}");
 }
